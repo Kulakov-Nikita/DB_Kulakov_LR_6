ff1288ec7d44744fbcfc019c619751b70e3e67e2
Кулаков Никита <kulakofnikita@gmail.com>
2023-05-16 15:29:52 +0300

    Добавлен выбор запроса к БД из списка

--- DB_Kulakov_LR_6/DB_Kulakov_LR_6.py
+++ DB_Kulakov_LR_6/DB_Kulakov_LR_6.py
@@ -1,7 +1,9 @@
-import sys
+﻿import sys
 import mysql.connector
 from mysql.connector import Error
+import mySQL_Queries
 
+# Соединить приложение и БД
 def create_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
@@ -17,6 +19,38 @@ def create_connection(host_name, user_name, user_password, db_name):
 
     return connection
 
-
 if __name__ == '__main__':
+    # Открыть доступ к БД
     dbConnection = create_connection("localhost", "root", "falcon27685", "mydb")
+
+    print("Доступные запросы к БД:")
+    print("[1] Получить информацию об автомобиле с информацией о модели")
+    print("[2] Получить информацию о производителе")
+    print("[3] Добавить отзыв")
+    print("[4] Добавить клиента")
+    print("[5] Изменить название услуги")
+    print("[6] Изменить телефон работника")
+    print("[7] Удалить деталь")
+    print("[8] Удалить автомобиль")
+    print("[9] Подобрать деталь, соответствующую данной модели автомобиля")
+    print("[10] Найти все автомобили данной модели")
+
+    choise=int(input("Выбирите ваш запрос: "))
+    
+    while choise < 1 or choise > 10:
+         choise=int(input("Запроса с таким номером нет списке. Попробуйте выбрать другой запрос: "))
+    
+    queriesDict={
+        1:mySQL_Queries.getCarAndModelInfo,
+        2:mySQL_Queries.getManufactorInfo,
+        3:mySQL_Queries.addReview,
+        4:mySQL_Queries.addUser,
+        5:mySQL_Queries.changeServiceName,
+        6:mySQL_Queries.changeWorkerPhone,
+        7:mySQL_Queries.deleteDetail,
+        8:mySQL_Queries.deleteCar,
+        9:mySQL_Queries.findDetailToModel,
+        10:mySQL_Queries.findCarsToModel,
+        }
+
+    queriesDict.get(choise)
---
+++ DB_Kulakov_LR_6/mySQL_Queries.py
@@ -0,0 +1,29 @@
+def getCarAndModelInfo():
+    pass
+
+def getManufactorInfo():
+    pass
+
+def addReview():
+    pass
+
+def addUser():
+    pass
+
+def changeServiceName():
+    pass
+
+def changeWorkerPhone():
+    pass
+
+def deleteDetail():
+    pass
+
+def deleteCar():
+    pass
+
+def findDetailToModel():
+    pass
+
+def findCarsToModel():
+    pass
