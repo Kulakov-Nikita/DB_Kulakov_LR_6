fbf93b1a615986249139b341c53208dab6ec9e8a
Кулаков Никита <kulakofnikita@gmail.com>
2023-05-16 14:49:07 +0300

    Добавленно подключение к БД

--- DB_Kulakov_LR_6/DB_Kulakov_LR_6.py
+++ DB_Kulakov_LR_6/DB_Kulakov_LR_6.py
@@ -1,1 +1,22 @@
+import sys
+import mysql.connector
+from mysql.connector import Error
 
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("\nConnection to MySQL DB successful\n")
+    except Error as e:
+        print(f"\nThe error '{e}' occurred\n")
+
+    return connection
+
+
+if __name__ == '__main__':
+    dbConnection = create_connection("localhost", "root", "falcon27685", "mydb")
